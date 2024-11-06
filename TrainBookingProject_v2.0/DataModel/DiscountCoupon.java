9719291485395ad79a4b4124f3b16eeb1cdfa086
Shu-yuan Li <lsy20001024@gmail.com>
2024-11-06 14:49:21 +0800

    Add coupon

---
+++ TrainBookingProject_v2.0/DataModel/DiscountCoupon.java
@@ -0,0 +1,37 @@
+
+package DataModel;
+
+import java.time.LocalDate;
+
+public class DiscountCoupon implements Coupon {
+    private double discount;
+    private String couponType;
+    private String couponCode;
+    private LocalDate expiryDate;
+
+    public DiscountCoupon(double discount, String couponType, String couponCode, LocalDate expiryDate) {
+        this.couponId = couponId;
+        this.discount = discount;
+        this.couponType = couponType;
+        this.couponCode = couponCode;
+        this.expiryDate = expiryDate;
+    }
+
+
+    public double getDiscount(double totalAmount) {
+        
+        return totalAmount * discount;
+    }
+
+    public String getCouponType() {
+        return couponType;
+    }
+
+    public String getCouponCode() {
+        return couponCode;
+    }
+
+    public boolean isExpired() {
+        return LocalDate.now().isAfter(expiryDate);
+    }
+}
