534415dfb3d45c83287c20ea85320640fcfb7930
Wong Yue Sing <157453744+ZzzWrong0209@users.noreply.github.com>
2024-11-18 12:14:16 +0800

    Update TrainTicketSystem.java
    CS function : import library

--- TrainBookingProject_v2.0/TrainTicketSystem.java
+++ TrainBookingProject_v2.0/TrainTicketSystem.java
@@ -1,6 +1,8 @@
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import DAO.*;
