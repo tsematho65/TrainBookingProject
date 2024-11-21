29eca0d6a883787a3cb46cf073a10c23441bfc16
Shu-yuan Li <lsy20001024@gmail.com>
2024-11-21 16:17:21 +0800

    remove AccountFactory

--- AccountFactory/AbstractFactory.java
+++
@@ -1,6 +0,0 @@
-package AccountFactory;
-
-public abstract class AbstractFactory {
-    public abstract Account createUser(String username, String password, String email, String phone);
-    public abstract Account createAdmin(String username, String password, String email, String phone);
-}
--- AccountFactory/AdminAccount.java
+++
@@ -1,9 +0,0 @@
-package AccountFactory;
-
-public class AdminAccount extends Account {
-    public AdminAccount(String username, String password, String email, String phone) {
-        super(username, password, email, phone, phone);
-        this.setType("admin");
-    }
-    // Additional admin-specific methods
-}
